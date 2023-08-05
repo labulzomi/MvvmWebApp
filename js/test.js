0096ca529e48640dec33476d9dfa30e00c3ea576
labulzomi <luca.bulzomi@isiskeynes.it>
2023-08-05 18:42:04 +0200

    login

---
+++ js/test.js
@@ -0,0 +1,1 @@
+//qui inserire le chiamate js
---
+++ php/login.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+</head>
+<body>
+<div>
+    
+    <div>
+        <div>
+            <span>LOGIN</span>
+        </div>
+        <div style="background-color:#ECF0F1">
+        </div>
+    </div>
+    <div>
+    </div>
+
+</div>
+</body>
+</html>
