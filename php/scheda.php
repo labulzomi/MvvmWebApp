da11551cca7cb89ff0a30eefc7f0e05623189c37
labulzomi <luca.bulzomi@isiskeynes.it>
2023-08-08 23:51:54 +0200

    .

--- php/scheda.php
+++ php/scheda.php
@@ -50,144 +50,66 @@ $dato=json_decode($_POST["dati"]);
     <div class="Contenitore">
 
         <div class="grigliaScheda">
-            <div  class="">
-                <img id="img_prof" src="
-                <?php               
-                
-                    if($dato->Foto==null)
-                    echo "http://areaverifica.altervista.org/GaleazziOnline/risorse/imgs/noimage.jpeg";
-                else
-                echo "http://areaverifica.altervista.org/Galeazzi/img/".$dato->Foto;
-  
-    ?>
-                "/>
+            
+        </div>
+        <div style="display:flex;justify-content: space-around;text-align:center;align-items: center;"> 
+            <fieldset>
+            <legend>Anagrafica:</legend>
+            <div>
+            <img id="img_prof" src="<?php               
+                        
+                            if($dato->Foto==null)
+                            echo "http://areaverifica.altervista.org/GaleazziOnline/risorse/imgs/noimage.jpeg";
+                        else
+                        echo "http://areaverifica.altervista.org/Galeazzi/img/".$dato->Foto;
+        
+            ?>"/>
             </div>
-            <div id="nominativo" class="">
-                <div>
-                    <input type="text" value="
-                    <?php     
-    echo trim(json_decode($_POST["dati"])->Nome);
-    ?>
-                    "/>
-                </div>
-                <div>
-                <input type="text" value="
-                    <?php     
-    echo trim(json_decode($_POST["dati"])->Cognome);
-    ?>
-                    "/>
-                </div>
-                
+            <div> <button value="">Cambia Immagine</button></div>
+            <div> <button value="">Rimuovi Immagine</button></div>
+            <div> <input type="text" value="<?php     
+            echo trim(json_decode($_POST["dati"])->Nome);
+            ?>"/></div>
+            <div> <input type="text" value="<?php echo trim(json_decode($_POST["dati"])->Cognome);?>"/></div>
+            </fieldset>
+            <fieldset>
+            <legend>Valutazioni:</legend>
+            <div>
+            <button value=""  >Aggiungi valutazione</button>
             </div>
-            <div id="valutazioni" class="">
-                <h3>Valutazioni</h3>
+            <div> 
                 <ul id="studentList">
                     <?php
-                    foreach($dato->Valutazioni as $val)
-                    {
-                        echo '<li>                       
-                        <span>Voto: '.$val->Voto.'</span>
-                        <span>Data: '.$val->Data.'</span>
-                        <button class="delete-button">Elimina</button>
-                        </li>';
-                    }
-                    
-                    
+                        foreach($dato->Valutazioni as $val)
+                        {
+                            echo '<li>
+                            <span><img width="30px" src="../risorse/imgs/voti.png"></span>
+                            <span> <select>';
+                            for($i=1;$i<=10;$i=$i+0.5){
+                                $f=$i==$val->Voto?"selected":"";
+                                echo "<option $f value=$i>$i</option>";
+                            }
+                            echo '</select></span>	                       
+                            <span><img width="30px" src="../risorse/imgs/calendar.png"></span>
+                            <span> <input type="date" value="'.$val->Data.'" /></span>
+                            
+                            <button class="delete-button"><img width="30px" src="../risorse/imgs/deleteicon.png"></button>
+                            </li>';
+                        }
+                            
+                            
                     ?>
-                    
-                </ul>
-                
-            </div>
+                                            
+                </ul></div>
+            
+        
+            </fieldset>
+
         </div>
-
-    <?php 
-    //var_dump($dato->Foto);
-
-    //echo "--- ".$dato->Foto;
-   // var_dump($_POST["dati"]);
+    <?php  
     ?>
 
-<div style="display:flex;justify-content: space-around;text-align:center"> 
-	<fieldset>
-	  <legend>Anagrafica:</legend>
-	  <div>
-	  <img id="img_prof" src="<?php               
-                
-                    if($dato->Foto==null)
-                    echo "http://areaverifica.altervista.org/GaleazziOnline/risorse/imgs/noimage.jpeg";
-                else
-                echo "http://areaverifica.altervista.org/Galeazzi/img/".$dato->Foto;
-  
-    ?>"/>
-	  </div>
-	 <div> <button value="">Cambia Immagine</button></div>
-     <div> <button value="">Rimuovi Immagine</button></div>
-	 <div> <input type="text" value="<?php     
-    echo trim(json_decode($_POST["dati"])->Nome);
-    ?>"/></div>
-	 <div> <input type="text" value="<?php echo trim(json_decode($_POST["dati"])->Cognome);?>"/></div>
-	 </fieldset>
-	<fieldset>
-	  <legend>Valutazioni:</legend>
-	  <div>
-	   <button value=""  >Aggiungi valutazione</button>
-	  </div>
-	 <div> 
-		<ul id="studentList">
-            <?php
-                foreach($dato->Valutazioni as $val)
-                {
-                    echo '<li>
-                    <span><img width="40px" src="../risorse/imgs/voti.png"></span>
-					<span> <select>';
-                    for($i=1;$i<=10;$i=$i+0.5){
-                        $f=$i==$val->Voto?"selected":"";
-                        echo "<option $f value=$i>$i</option>";
-                    }
-                    echo '</select></span>	                       
-                    <span><img width="40px" src="../risorse/imgs/calendar.png"></span>
-                    <span> <input type="date" value='.$val->Data.'/></span>
-                    
-                    <button class="delete-button">Elimina</button>
-                    </li>';
-                }
-                    
-                    
-            ?>
-                    <li>                       
-                        <span><img width="40px" src="C:\Users\sonlu\Desktop\MvvmWebApp\MvvmWebApp\risorse\imgs\voti.png"></span>
-						<span> <select><option></option></select></span>
-                        <span><img width="40px" src="C:\Users\sonlu\Desktop\MvvmWebApp\MvvmWebApp\risorse\imgs\calendar.png"></span>
-						<span> <input type="date"/></span>
-                        <button class="delete-button">Elimina</button>
-                        </li>
-					<li>                       
-                        <span><img width="40px" src="C:\Users\sonlu\Desktop\MvvmWebApp\MvvmWebApp\risorse\imgs\voti.png"></span>
-						<span> <select><option></option></select></span>
-                        <span><img width="40px" src="C:\Users\sonlu\Desktop\MvvmWebApp\MvvmWebApp\risorse\imgs\calendar.png"></span>
-						<span> <input type="date"/></span>
-                        <button class="delete-button">Elimina</button>
-                        </li>
-					<li>                       
-                        <span><img width="40px" src="C:\Users\sonlu\Desktop\MvvmWebApp\MvvmWebApp\risorse\imgs\voti.png"></span>
-						<span> <select><option></option></select></span>
-                        <span><img width="40px" src="C:\Users\sonlu\Desktop\MvvmWebApp\MvvmWebApp\risorse\imgs\calendar.png"></span>
-						<span> <input type="date"/></span>
-                        <button class="delete-button">Elimina</button>
-                        </li>
-					<li>                       
-                        <span><img width="40px" src="C:\Users\sonlu\Desktop\MvvmWebApp\MvvmWebApp\risorse\imgs\voti.png"></span>
-						<span> <select><option></option></select></span>
-                        <span><img width="40px" src="C:\Users\sonlu\Desktop\MvvmWebApp\MvvmWebApp\risorse\imgs\calendar.png"></span>
-						<span> <input type="date"/></span>
-                        <button class="delete-button">Elimina</button>
-                        </li>                    
-		</ul></div>
-	  
- 
-	 </fieldset>
 
- </div>
         <div id="statistiche">
                         
         </div>
