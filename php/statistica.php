f5ae1cf60d3ce6374e383e98fbc234a61a37c958
labulzomi <luca.bulzomi@isiskeynes.it>
2023-08-28 18:45:50 +0200

    .

--- php/statistica.php
+++ php/statistica.php
@@ -172,6 +172,7 @@ class Statistica
 
         $dati->Date = $sortedDates;
         $dati->Valutazioni = $valutazioniAllineate;
+        var_dump($dati->Valutazioni);
         return $dati;
     }
 
